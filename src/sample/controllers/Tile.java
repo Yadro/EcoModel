17476dbcd40b9a5004f32e4fe4e29a0290cbf291
Space <yadrone@gmail.com>
2015-04-30 10:19:34 +0300

    Rename the tile.

--- src/sample/controllers/Tile.java
+++ src/sample/controllers/Tile.java
@@ -4,7 +4,7 @@ import javafx.scene.image.Image;
 
 interface Tile {
     int TILE_SIZE = 45;
-    Image wolf = new Image("wolf_vec2.png");
+    Image wolf = new Image("wolf.png");
     Image rabbit = new Image("rabbit.png");
     Image play = new Image("play.png");
     Image next = new Image("next.png");
